760a61a07176aa7838da60d1fee4493f086a5619
Wichi <geraldorincon52@gmail.com>
2025-10-17 22:26:42 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch'
 
 const handler = async (m, { conn, text, command, usedPrefix}) => {
-  const apikey = "sylphy-e321"
+  const apikey = "sylphy-8238wss"
 
   if (!text) {
     return m.reply(`📌 *Uso correcto:*\n${usedPrefix + command} <URL de YouTube>\n📍 *Ejemplo:* ${usedPrefix + command} https://youtube.com/watch?v=abc123`)
@@ -13,26 +13,28 @@ const handler = async (m, { conn, text, command, usedPrefix}) => {
 }
 
   try {
-    const res = await fetch(`https://api.sylphy.xyz/download/ytmp3?url=${encodeURIComponent(text)}&apikey=sylphy-e321`)
+    const res = await fetch(`https://api.sylphy.xyz/download/ytmp3?url=${encodeURIComponent(text)}&apikey=sylphy-8238wss`)
     const json = await res.json()
 
-    if (!json.status ||!json.data ||!json.data.dl_url) {
+    if (!json.status ||!json.res ||!json.res.url) {
       return m.reply("❌ No se pudo descargar el audio.")
 }
 
-    const info = json.data
+    const info = json.res
     const caption = `
 ╭─🎶 *YouTube MP3 Downloader* 🎶─╮
 │
 │ 🎵 *Título:* ${info.title}
-│ ⏱️ *Duración:* ${info.duration}
+│ 💽 *Formato:* ${info.format}
+│ 🔊 *Calidad:* ${info.quality}
+│ 📦 *Tamaño:* ${info.filesize}
 │ 📥 *Descargando audio...*
 ╰────────────────────────────╯
 `
 
     await conn.sendMessage(m.chat, { image: { url: info.thumbnail}, caption}, { quoted: m})
     await conn.sendMessage(m.chat, {
-      audio: { url: info.dl_url},
+      audio: { url: info.url},
       mimetype: 'audio/mp4',
       fileName: `${info.title}.mp3`
 }, { quoted: m})
