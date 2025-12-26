2b93b55bf65684b53bde7c2b663dc1533bb63398
Wichi <barbozasebastian408@gmail.com>
2025-12-26 19:36:29 -0400

    Actualizar descargas-likee.js

--- plugins/descargas-likee.js
+++ plugins/descargas-likee.js
@@ -1,20 +1,33 @@
-//Para Usuarios De Likee 🙃
-import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 Ingrese la url de un video de *Likee*.', m, rcanal)
-await m.react('🕓')
-try {
-let app = await fetch(`https://apis-starlights-team.koyeb.app/starlight/like-downloader?url=${text}`, { headers: { 'Content-Type': 'application/json' }})
-let json = await app.json()
-let video = json.links['no watermark']
-await conn.sendFile(m.chat, video, 'samu.mp4', `*✔️🍟Downloader likee.*`, m)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['likee *<url>*']
-handler.tags = ['dl']
-handler.command = /^(likee)$/i
-handler.register = false
+
+let handler = async (m, { args, command, isOwner}) => {
+  const chatId = m.chat
+
+  if (!isOwner) {
+    return m.reply('🚫 Solo mi dueño puede usar este comando.')
+}
+
+  if (!args[0]) {
+    return m.reply(`⚙️ Usa:\n\n${command} on  – para activar el bot\n${command} off – para desactivarlo`)
+}
+
+  const option = args[0].toLowerCase()
+
+  if (option === 'on') {
+    botStatus[chatId] = true
+    m.reply('✅ Bot activado en este grupo.')
+} else if (option === 'off') {
+    botStatus[chatId] = false
+    m.reply('🛑 Bot desactivado en este grupo.')
+} else {
+    m.reply('❓ Opción no válida. Usa "on" o "off".')
+}
+}
+
+handler.help = ['bot <on/off>']
+handler.tags = ['owner']
+handler.command = /^bot$/i
+handler.group = true
+handler.rowner = true // Solo dueños reales
+
 export default handler
