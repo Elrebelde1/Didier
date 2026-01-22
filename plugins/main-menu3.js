ac24434ef34603365fb779c5d64b3d81f079828f
Wichi <barbozasebastian408@gmail.com>
2026-01-22 17:55:28 -0400

    Actualizar main-menu3.js

--- plugins/main-menu3.js
+++ plugins/main-menu3.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const handler = async (m, { isPrems, conn }) => {
   const last = global.db.data.users[m.sender].lastcofre || 0
   const now = new Date() * 1
@@ -5,65 +7,65 @@ const handler = async (m, { isPrems, conn }) => {
 
   if (now - last < cooldown) {
     const wait = msToTime((last + cooldown) - now)
-    throw `⏳ 𝖤𝗅 𝗌𝗂𝗌𝗍𝖾𝗆𝖺 𝖾𝗌𝗍𝖺́ 𝗉𝗋𝗈𝖼𝖾𝗌𝖺𝗇𝖽𝗈... 𝖤𝗌𝗉𝖾𝗋𝖺 *${wait}*.`
+    throw `⏳ El sistema está procesando... Espera *${wait}*.`
   }
 
-  // Imagen principal de Vans Bot
-  const img = 'https://files.catbox.moe/dcp02s.jpg' 
-  
+  // Nueva Imagen de Didier Bot
+  const img = 'https://files.catbox.moe/dpofx7.jpg' 
+
   const texto = `
 ╭╾━━━━╼ 〔 🎨 〕 ╾━━━━╼╮
-│  👟 *𝖁𝖆𝖓𝖘 𝕭𝖔𝖙 𝕯𝖎𝖘𝖊𝖓̃𝖔𝖘*
-│
-│  🛸 *𝖦𝗋𝖺𝗉𝗁𝗂𝖼 𝖲𝗒𝗌𝗍𝖾𝗆*
-│  🛹 *𝖡𝗒 𝖤𝗅𝗂𝗎𝖽*
-│
-│  *ᴇsᴛɪʟᴏs ᴅᴇ ᴛᴇxᴛᴏ:*
-│  ◦ .logoneon
-│  ◦ .logoglitch
-│  ◦ .logograffiti3d
-│  ◦ .logomatrix
-│  ◦ .logofuturista
-│  ◦ .logocielo
-│
-│  *ɢᴀᴍɪɴɢ & ᴘᴇʀsᴏɴᴀᴊᴇs:*
-│  ◦ .logogaming
-│  ◦ .logonaruto
-│  ◦ .logodragonball
-│  ◦ .logoarmy
-│  ◦ .logopubg
-│  ◦ .logopubgfem
-│  ◦ .logoguerrero
-│  ◦ .logolol
-│  ◦ .logoamongus
-│
-│  *ᴇғᴇᴄᴛᴏs ʏ ʀᴇᴅᴇs:*
-│  ◦ .tweet
-│  ◦ .sadcat
-│  ◦ .logocorazon
-│  ◦ .logopareja
-│  ◦ .logoalas
-│  ◦ .logonube
-│  ◦ .logohorror
-│
-│  *ᴍᴜʟᴛɪᴍᴇᴅɪᴀ:*
-│  ◦ .logoportadaplayer
-│  ◦ .logoportadaff
-│  ◦ .logovideotiger
-│  ◦ .logovideointro
-│  ◦ .logovideogaming
-│
-╰╾━━━━╼ 〔 🛸 〕 ╾━━━━╼╯
-*𝖡𝗒 𝖤𝗅𝗂𝗎𝖽 • 𝖵𝖺𝗇𝗌 𝖡𝗈𝗍*`.trim()
+┃  ✨ *𝐃𝐈𝐃𝐈𝐄𝐑 𝐁𝐎𝐓 𝐃𝐈𝐒𝐄𝐍̃𝐎𝐒*
+┃
+┃  🚀 *𝐆𝐫𝐚𝐩𝐡𝐢𝐜 𝐒𝐲𝐬𝐭𝐞𝐦*
+┃  ⚡ *𝐁𝐲 𝐃𝐢𝐝𝐢𝐞𝐫 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬*
+┃
+┃  *ᴇsᴛɪʟᴏs ᴅᴇ ᴛᴇxᴛᴏ:*
+┃  ◦ .logoneon
+┃  ◦ .logoglitch
+┃  ◦ .logograffiti3d
+┃  ◦ .logomatrix
+┃  ◦ .logofuturista
+┃  ◦ .logocielo
+┃
+┃  *ɢᴀᴍɪɴɢ & ᴘᴇʀsᴏɴᴀᴊᴇs:*
+┃  ◦ .logogaming
+┃  ◦ .logonaruto
+┃  ◦ .logodragonball
+┃  ◦ .logoarmy
+┃  ◦ .logopubg
+┃  ◦ .logopubgfem
+┃  ◦ .logoguerrero
+┃  ◦ .logolol
+┃  ◦ .logoamongus
+┃
+┃  *ᴇғᴇᴄᴛᴏs ʏ ʀᴇᴅᴇs:*
+┃  ◦ .tweet
+┃  ◦ .sadcat
+┃  ◦ .logocorazon
+┃  ◦ .logopareja
+┃  ◦ .logoalas
+┃  ◦ .logonube
+┃  ◦ .logohorror
+┃
+┃  *ᴍᴜʟᴛɪᴍᴇᴅɪᴀ:*
+┃  ◦ .logoportadaplayer
+┃  ◦ .logoportadaff
+┃  ◦ .logovideotiger
+┃  ◦ .logovideointro
+┃  ◦ .logovideogaming
+┃
+╰╾━━━━╼ 〔 🚀 〕 ╾━━━━╼╯
+*By Didier Developers • 𝐃𝐈𝐃𝐈𝐄𝐑 𝐁𝐎𝐓*`.trim()
 
   await conn.sendMessage(m.chat, { image: { url: img }, caption: texto }, { quoted: m })
 
   global.db.data.users[m.sender].lastcofre = now
 }
 
-handler.help = ['menu3']
+handler.help = ['menu3', 'logos']
 handler.tags = ['main', 'logo']
-handler.command = ['menulogos', 'logos', 'menu3'] 
+handler.command = ['menulogos', 'logos', 'menu3', 'disaños'] 
 
 export default handler
 
